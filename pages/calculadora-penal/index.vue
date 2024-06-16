ce8a658e93dd396e698eb1a6f6d57f7a9a5c14e5
Gustavo Cabreira <gustavo.softdev@gmail.com>
2024-06-16 11:53:03 -0300

    added tailwind

---
+++ pages/calculadora-penal/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="flex justify-center items-center h-screen">
+    <h1 class="text-xl font-bold text-blue-500">
+      Hello, Tailwind CSS with Nuxt.js!
+    </h1>
+  </div>
+</template>
